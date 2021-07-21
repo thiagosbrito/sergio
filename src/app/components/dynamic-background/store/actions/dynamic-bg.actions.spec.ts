5c2c833b4c9fd0329df17e8bca1ccd5a83184d9e
Thiago Brito <thiago.brito@dtn.com>
2021-07-21 20:16:25 +0200

    Auto-generated commit [ci skip]

--- src/app/components/dynamic-background/store/actions/dynamic-bg.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromDynamicBG from './dynamic-bg.actions';
-
-describe('loadDynamicBGs', () => {
-  it('should return an action', () => {
-    expect(fromDynamicBG.loadDynamicBGs().type).toBe('[DynamicBG] Load DynamicBGs');
-  });
-});
--- src/app/gallery/store/gallery.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromGallery from './gallery.actions';
-
-describe('loadGallerys', () => {
-  it('should return an action', () => {
-    expect(fromGallery.loadGallerys().type).toBe('[Gallery] Load Gallerys');
-  });
-});
